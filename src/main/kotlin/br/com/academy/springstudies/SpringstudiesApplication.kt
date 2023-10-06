ca173fe2ad50de87bda9b9a799c775eb653caac7
Nesomar Ramalho <nesomar.ramalho@gmail.com>
2023-10-06 20:41:33 -0300

    estrutura inicial

---
+++ src/main/kotlin/br/com/academy/springstudies/SpringstudiesApplication.kt
@@ -0,0 +1,11 @@
+package br.com.academy.springstudies
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringstudiesApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringstudiesApplication>(*args)
+}
